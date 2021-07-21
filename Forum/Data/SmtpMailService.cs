6467df604635468932f5c37568997a00b2e8dc38
Colin van Loo <colin.vanloo@ch.zetes.com>
2021-07-21 15:47:44 +0200

    feat(mail): Implemented mail service.

---
+++ Forum/Data/SmtpMailService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace Forum.Data
+{
+	public class SmtpMailService : IMailService
+	{
+		public async Task Send(Model.Message message)
+		{
+			using (var smtp = new SmtpClient())
+			{
+				smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+				smtp.PickupDirectoryLocation = @"C:\tmp\mail";
+
+				MailMessage mail = new MailMessage()
+				{
+					Body = message.Body,
+					Subject = message.Subject,
+					From = new MailAddress(message.From)
+				};
+				mail.To.Add(string.Join(';', message.To));
+
+				await smtp.SendMailAsync(mail);
+			}
+		}
+	}
+}
